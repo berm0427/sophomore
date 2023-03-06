951b0f7f89e163a118e7fa7e18ae606eb68271bc
berm0427 <berm0427@naver.com>
2023-03-06 11:00:03 +0900

    Update MBSC1.cpp

--- 시스템프로그래밍/MBSC1.cpp
+++ 시스템프로그래밍/MBSC1.cpp
@@ -3,13 +3,13 @@
 */
 #include <stdio.h>
 #include <string.h>
-#include <stddef.h> // 교재에는 없지만 이게 있어야 실행 가능
+#include <stddef.h> // 교재에는 없지만 이게 있어야 실행 가능 (visual studio(윈도우 기반 C)에서는 자동으로 가지고 와서 필요가 없는데 다른데에서는 여기에 넣어주어야..)
 
 int main(void)
 {
-    wchar_t str[] = L"ABC";
+    wchar_t str[] = L"ABC"; //여기서 문제가 발생한다고 한다
     int size = sizeof(str);
-    int len = strlen(str);
+    int len = strlen(str); // WBCS 기반의 문자열 함수로 변경을 해주어야 한다
 
 
     printf("배열의 크기 : %d \n", size);
