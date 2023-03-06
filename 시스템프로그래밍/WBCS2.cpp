80e8ae15f1fa8a6f6dad5efc1d4f4de9fc3ef8f1
berm0427 <berm0427@naver.com>
2023-03-06 11:11:53 +0900

    Update WBCS2.cpp

--- 시스템프로그래밍/WBCS2.cpp
+++ 시스템프로그래밍/WBCS2.cpp
@@ -6,7 +6,7 @@
 #include <stddef.h> // 교재에는 없지만 이게 있어야 실행 가능 (visual studio(윈도우 기반 C)에서는 자동으로 가지고 와서 필요가 없는데 다른데에서는 여기에 넣어주어야..)
 #include <locale.h>
 
-int main(void)
+int main(int argc, char* argc[]// 뒤에 문자를 받아서 프로그램에 쓸때 사용)
 {
     _wsetlocale(LC_ALL,L"korean");
     wchar_t str[] = L"ABC한글";
