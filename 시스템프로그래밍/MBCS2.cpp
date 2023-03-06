106e5457c9e3f091782b7833713519f10c57f174
berm0427 <berm0427@naver.com>
2023-03-06 11:02:41 +0900

    Update MBCS2.cpp

--- 시스템프로그래밍/MBCS2.cpp
+++ 시스템프로그래밍/MBCS2.cpp
@@ -3,7 +3,7 @@
 */
 #include <stdio.h>
 #include <string.h>
-#include <stddef.h> //
+#include <stddef.h> // 교재에는 없지만 이게 있어야 실행 가능 (visual studio(윈도우 기반 C)에서는 자동으로 가지고 와서 필요가 없는데 다른데에서는 여기에 넣어주어야..)
 
 int main(void)
 {
