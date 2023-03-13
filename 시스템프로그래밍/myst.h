ec9bc4293d3e5044f7e899266c0ba2e79fabcf34
berm0427 <berm0427@naver.com>
2023-03-13 10:31:26 +0900

    Create myst.h

---
+++ 시스템프로그래밍/myst.h
@@ -0,0 +1,7 @@
+#pragma once //컴파일 과정 시 단 한번만 실행하라는 명령
+
+#define SNO 20223595 //정수형 상수 정의
+#define NAME "유창범" //문자형 상수 정의
+#define PRINT printf("%c", op)// 실행문 정의
+#define GOB(a) (a) * (a) //매크로 정의
+// 매크로 정의시에는 ;이 없다
