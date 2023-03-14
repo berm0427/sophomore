7c3be7a7f967edb0d17c82840dc614e4c128f490
berm0427 <berm0427@naver.com>
2023-03-14 23:18:38 +0900

    Update fibonachi?1.c
    벙찐 오류 수정

--- 자료구조/fibonachi?1.c
+++ 자료구조/fibonachi?1.c
@@ -9,6 +9,7 @@ int main()
 
     int* arr = malloc(sizeof(int) * size); // 원하는 사이즈로 할당해준다 (int 4byte * 원하는 크기로 해야 정상작동)
     int sum = 0;
+    printf("[>] 숫자 %d개 입력:\n[<] ", size);
     for (int i = 0; i < size; i++)
     {
         scanf("%d", &arr[i]);
@@ -16,9 +17,9 @@ int main()
 
     for (int i = 0; i < size; i++)
     {
-        sum = sum+arr[i];
+        sum = sum + arr[i];
     }
 
-    printf("[>] 피보나치 계산 결과는 %d", sum);
+    printf("[>] 계산 결과는 %d", sum);
     free(arr);
 }
