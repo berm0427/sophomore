5ffe83162ada07a0208ca1c44e76519a4e312f25
berm0427 <berm0427@naver.com>
2023-10-12 19:13:00 +0900

    Update array_with_dimension.cs

--- 윈도우프로그래밍/array_with_dimension.cs
+++ 윈도우프로그래밍/array_with_dimension.cs
@@ -4,8 +4,15 @@ int[] Array2 = new int[] { 1, 2, 3, 4, 5, 6, 7 };
 
 foreach (int cnt in Array1)
 {
-    Console.WriteLine(cnt + " ");
+    Console.Write(cnt + " ");
 }
+Console.WriteLine();
+
+foreach(int cnt in Array2)
+{
+    Console.Write(cnt + " ");
+}
+Console.WriteLine("\n");
 
 int[,] number2D = new int[3,2] {{1, 3}, {5, 7}, {6, 8}};
 Console.WriteLine();
@@ -21,4 +28,7 @@ int d1 = Array_Rank1.Rank;
 int d2 = Array_Rank2.Rank;
 
 Console.WriteLine(" > 차원 수 : " + d1);
+Console.WriteLine(" > 차원 수 : " + d2);
+
+
 Console.WriteLine(" > 차원 수 : " + d2);
