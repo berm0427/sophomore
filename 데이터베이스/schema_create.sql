6e3fa2936aef0d2dcac4541cf6f8d341ddfd3b5d
berm0427 <berm0427@naver.com>
2023-10-13 10:32:32 +0900

    Create schema_create.sql

---
+++ 데이터베이스/schema_create.sql
@@ -0,0 +1,57 @@
+drop database if exists Movie;
+create database if not exists Movie;
+
+-- SQL 명령어를 실행할 대상인 기본 데이터베이스를 univDB로 지정
+use Movie;
+
+
+CREATE TABLE Movie
+(
+  title varchar(25),
+  year int,
+  length int,
+  inColor int,
+  studioName varchar(15),
+  producerC varchar(3) references MovieExec(cert),
+  primary key (title, year)
+);
+
+  
+
+CREATE TABLE MovieStar
+(
+  name varchar(30),
+  address varchar(30),
+  gender varchar(1),
+  birthdate varchar(10),
+  primary key (name)
+);
+
+  
+  CREATE TABLE StarsIn 
+(
+  movieTitle varchar(30),
+  movieYear int,
+  starName varchar(30) references MovieStar(name),
+  primary key(movieTitle),
+  foreign key (movieTitle, movieYear) references Movie(title,year)
+);
+
+CREATE TABLE MovieExec
+(
+  name varchar(30),
+  address varchar(30),
+  cert int,
+  netWorth int,
+  primary key(cert)
+);
+
+CREATE TABLE Studio
+(
+  name varchar(30),
+  address varchar(30),
+  presC varchar(3) references MovieExec(cert),
+  primary key(name)
+);
+
+  SET foreign_key_checks = 0; -- 외래키 체크 끄기
