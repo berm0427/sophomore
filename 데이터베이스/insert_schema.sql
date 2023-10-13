11cfcc7e935799e2ab42dd468290565979c9dd4c
berm0427 <berm0427@naver.com>
2023-10-13 11:21:13 +0900

    Create insert_schema.sql

---
+++ 데이터베이스/insert_schema.sql
@@ -0,0 +1,34 @@
+INSERT INTO MovieExec VALUES('George Lucas','Oak Rd.',555,200000000);
+INSERT INTO MovieExec VALUES('Ted Turner','Turner Av.',333,125000000);
+INSERT INTO MovieExec VALUES('Stephen Spielberg','123 ET road',222,100000000);
+INSERT INTO MovieExec VALUES('Merv Griffin','Riot Rd.',199,112000000);
+INSERT INTO MovieExec VALUES('Calvin Coolidge','Fast Lane',123,20000000);
+
+INSERT INTO Movie VALUES('Pretty Woman',1990,119,1,'Disney',555);
+INSERT INTO Movie VALUES('The Man Who Wasn''t There', 2001,116,0,'USA Entertainm.',333);
+INSERT INTO Movie VALUES('Logan''s run',1976,NULL,1,'',123);
+INSERT INTO Movie VALUES('Star Wars',1977,124,1,'Fox',NULL);
+INSERT INTO Movie VALUES('Empire Strikes Back',1980,111,1,'Fox',555);
+INSERT INTO Movie VALUES('Star Trek',1979,132,1,'Paramount',333);
+INSERT INTO Movie VALUES('Star Trek: Nemesis',2002,116,1,'Paramount',123);
+INSERT INTO Movie VALUES('Terms of Endearment',1983,132,1,'MGM',123);
+INSERT INTO Movie VALUES('The Usual Suspects',1995,106,1,'MGM',199);
+INSERT INTO Movie VALUES('Gone With the Wind',1938,238,1,'MGM',123);
+INSERT INTO MovieStar VALUES('Jane Fonda','Turner Av.','F','7/7/77');
+INSERT INTO MovieStar VALUES('Alec Baldwin','Baldwin Av.','M','6/7/77');
+INSERT INTO MovieStar VALUES('Kim Basinger','Baldwin Av.','F','5/7/79');
+INSERT INTO MovieStar VALUES('Harrison Ford','Prefect Rd.','M','5/5/55');
+INSERT INTO MovieStar VALUES('Debra Winger','A way','F','5/6/78');
+INSERT INTO MovieStar VALUES('Jack Nicholson','X path','M','5/5/49');
+INSERT INTO MovieStar VALUES('Carrie Fisher','X path','M','5/5/49');
+INSERT INTO MovieStar VALUES('Mark Hamill','X path','M','5/5/49');
+INSERT INTO MovieStar VALUES('Kevin Spacey','X path','M','5/5/49');
+
+INSERT INTO StarsIn VALUES('Star Wars',1977,'Carrie Fisher');
+INSERT INTO StarsIn VALUES('Star Wars',1977,'Mark Hamill');
+INSERT INTO StarsIn VALUES('Star Wars',1977,'Harrison Ford');
+INSERT INTO StarsIn VALUES('Empire Strikes Back',1980,'Harrison Ford');
+INSERT INTO StarsIn VALUES('The Usual Suspects',1995,'Kevin Spacey');
+INSERT INTO StarsIn VALUES('Terms of Endearment',1983,'Debra Winger');
+INSERT INTO StarsIn VALUES('Terms of Endearment',1983,'Jack Nicholson');
+
