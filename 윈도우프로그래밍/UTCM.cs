9c7a5cb037abef57239409f3016b94ff5c5dcfd1
berm0427 <berm0427@naver.com>
2023-11-06 13:02:11 +0900

    Update UTCM.cs

--- 윈도우프로그래밍/UTCM.cs
+++ 윈도우프로그래밍/UTCM.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Windows.Forms;
 using TimeZoneConverter;
 
@@ -9,17 +10,27 @@ namespace TimeZoneApp
         private Label timeLabel;
         private TextBox countryTextBox;
         private Timer timer;
+        private Dictionary<string, string> countryToTimezoneMap;
 
         public MainForm()
         {
+            // Initialize the dictionary with some country-timezone pairs.
+            countryToTimezoneMap = new Dictionary<string, string>
+            {
+                { "Korea", "Asia/Seoul" },
+                // Add more countries and their respective timezone IDs here.
+                { "USA", "America/New_York" }, // Example for multiple entries
+                { "Japan", "Asia/Tokyo" },
+                { "DPRK", "Asia/Pyongyang"}
+            };
 
-            timeLabel = new Label() { Location = new System.Drawing.Point(10, 10), Width = 300 };
+            timeLabel = new Label() { Location = new System.Drawing.Point(10, 10), Size = new System.Drawing.Size(300, 20) };
             countryTextBox = new TextBox() { Location = new System.Drawing.Point(10, 40), Width = 200 };
             timer = new Timer() { Interval = 1000 };
 
             this.Controls.Add(timeLabel);
             this.Controls.Add(countryTextBox);
-            this.Controls.Add(new Label() { Location = new System.Drawing.Point(10, 70), Text = "Enter a country and press Enter" });
+            this.Controls.Add(new Label() { Location = new System.Drawing.Point(10, 70), Size = new System.Drawing.Size(300, 20), Text = "Enter a country name and press Enter" });
 
             countryTextBox.KeyDown += CountryTextBox_KeyDown;
             timer.Tick += Timer_Tick;
@@ -38,38 +49,44 @@ namespace TimeZoneApp
             if (e.KeyCode == Keys.Enter)
             {
                 UpdateTime();
-                e.SuppressKeyPress = true;
+                e.SuppressKeyPress = true; // To prevent ding sound on pressing Enter
             }
         }
 
         private void UpdateTime()
         {
-            string country = countryTextBox.Text;
+            string countryInput = countryTextBox.Text.Trim();
             DateTime currentTime = DateTime.UtcNow;
 
-            if (!string.IsNullOrWhiteSpace(country))
+            if (!string.IsNullOrWhiteSpace(countryInput))
             {
                 try
                 {
-                    string timeZoneId = TZConvert.GetTimeZoneInfo(country).Id;
-                    currentTime = TimeZoneInfo.ConvertTimeFromUtc(currentTime, TimeZoneInfo.FindSystemTimeZoneById(timeZoneId));
+                    if (countryToTimezoneMap.TryGetValue(countryInput, out var timezoneId))
+                    {
+                        TimeZoneInfo timeZoneInfo = TZConvert.GetTimeZoneInfo(timezoneId);
+                        currentTime = TimeZoneInfo.ConvertTimeFromUtc(currentTime, timeZoneInfo);
+                    }
+                    else
+                    {
+                        timeLabel.Text = "Invalid country name or timezone not found";
+                        return;
+                    }
                 }
-                catch
+                catch (Exception ex)
                 {
-                    timeLabel.Text = "Invalid country name or timezone not found";
+                    // Handle any unexpected error here
+                    timeLabel.Text = "Error: " + ex.Message;
                     return;
                 }
             }
 
-            timeLabel.Text = $"{country} Time: {currentTime:yyyy-MM-dd HH:mm:ss}";
+            timeLabel.Text = $"{countryInput} Time: {currentTime:yyyy-MM-dd HH:mm:ss}";
         }
     }
 
     static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main()
         {
@@ -79,4 +96,3 @@ namespace TimeZoneApp
         }
     }
 }
-
